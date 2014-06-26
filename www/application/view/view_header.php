02694a43914443ddd42437864d25e52289fc7f5b
Kolyabis <2z.dizayn@gmail.com>
2014-06-26 16:31:23 +0300

    header

--- www/application/view/view_header.php
+++ www/application/view/view_header.php
@@ -1,5 +1,11 @@
 <div>
 	<p>
 		ETO HEADER
+        <?php
+                    foreach($data as $row){
+                        echo '<pre>';
+                        print_r($data);
+                    }
+                ?>
 	</p>
 </div>
