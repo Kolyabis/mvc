93d6809a07041a24924a04b6ef1909928162273a
Kolyabis <2z.dizayn@gmail.com>
2014-06-26 16:36:12 +0300

    templates

--- www/application/view/view_header.php
+++ www/application/view/view_header.php
@@ -2,6 +2,7 @@
 	<p>
 		ETO HEADER
         <?php
+                    //Разбераем массив
                     foreach($data as $row){
                         echo '<pre>';
                         print_r($data);
