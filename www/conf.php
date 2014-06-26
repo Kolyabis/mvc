934cb9a8d280c7b622bd5340d4d477113886936e
Kolyabis <2z.dizayn@gmail.com>
2014-06-26 16:30:47 +0300

    26.06.2014 добавил класс подключение DB  PDO

--- www/conf.php
+++ www/conf.php
@@ -2,10 +2,10 @@
 	ini_set('display_errors', 1);
 	define('PS', PATH_SEPARATOR);
 	define('DS', DIRECTORY_SEPARATOR);
-	define('DB_USER', '2zdizayn');
-	define('DB_PASS', 'konstantin82');
-	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'mybase');
+	//define('DB_USER', '2zdizayn');
+	//define('DB_PASS', 'konstantin82');
+	//define('DB_HOST', 'localhost');
+	//define('DB_NAME', 'mybase');
 	set_include_path(get_include_path(). PS ."application ". DS ." core". DS ."route.php"									   
 									   . PS ."application ". DS ." core". DS ."controller_main.php"
 									   . PS ."application ". DS ." core". DS ."model_main.php"
@@ -13,5 +13,24 @@
 	require_once 'application/core/route.php';
 	//require_once 'application/core/controller_main.php';
 	//require_once 'application/core/model_main.php';	
-	require_once 'application/core/view_main.php';	
+	require_once 'application/core/view_main.php';
+    class Db_ext {
+        protected static $dsn = 'mysql:dbname=mybase;host=localhost';
+        protected static $user = '2zdizayn';
+        protected static $password = 'konstantin82';
+        protected static $_instance;
+            public static function getInstance(){
+                if(self::$_instance){
+                    return self::$_instance;
+                }else{
+                    try{
+                    return self::$_instance = new PDO(self::$dsn,self::$user,self::$password);
+                    }catch(Exception $error){
+                        echo $error->getCode().'<br>';
+                        echo $error->getMessage().'<br>';
+                        echo $error->getLine().'<br>';
+                    }
+                }
+            }
+    }
 ?>
